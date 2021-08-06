75b72f25fc5c7487353f7662fb44a3aa279a0945
Drew Powers <drew@pow.rs>
2021-08-06 15:56:22 -0600

    wip

--- packages/astro/src/preview/index.ts
+++ packages/astro/src/preview/index.ts
@@ -13,7 +13,7 @@ interface PreviewOptions {
 }
 
 /** The primary dev action */
-export default async function preview(config: AstroConfig, { logging }: PreviewOptions) {
+export async function preview(config: AstroConfig, { logging }: PreviewOptions) {
   const {
     dist,
     devOptions: { hostname, port },
